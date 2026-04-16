7f7346ee52820acd7f20c8a26ec5ce3ea5e52dac
shafqat1841 <shafqat1841@gmail.com>
2026-04-16 09:10:50 +0500

    chore: handle all errors in consumer creation time

--- src/entities_system/consumer.rs
+++ src/entities_system/consumer.rs
@@ -6,6 +6,7 @@ use std::{
 };
 
 use crossbeam::channel::Receiver;
+use thiserror::Error;
 
 use crate::entities_system::{
     app_global_state::GlobalState, consumer::consumer_task::ConsumerTask,
@@ -16,11 +17,17 @@ pub struct Consumer {
     handler: JoinHandle<()>,
 }
 
+#[derive(Error,Debug)]
+pub enum ConsumerErr {
+    #[error("Error in creating the handler")]
+    HandlerError,
+}
+
 impl Consumer {
     pub fn new(
         guarded_global_state: Arc<GlobalState>,
         producer_rx: Receiver<ProducerChannelData>,
-    ) -> Self {
+    ) -> Result<Self,ConsumerErr> {
         let producer_rx_clone = producer_rx.clone();
         let guarded_global_state_clone = guarded_global_state.clone();
         let task = move || match ConsumerTask::new(guarded_global_state_clone, producer_rx_clone) {
@@ -34,10 +41,15 @@ impl Consumer {
 
         let handler = thread::Builder::new()
             .name("consumer thread".to_string())
-            .spawn(task)
-            .unwrap();
+            .spawn(task);
 
-        Consumer { handler }
+        match handler {
+            Err(err) => {
+                println!("Error in creating the handler: {}", err);
+                return Err(ConsumerErr::HandlerError)
+            }
+            Ok(value) => Ok(Consumer { handler: value }),
+        }
     }
 
     pub fn check_threads_finished(&self) -> bool {
@@ -45,6 +57,11 @@ impl Consumer {
     }
 
     pub fn join_thread(self) {
-        self.handler.join().unwrap()
+        match self.handler.join() {
+            Err(err) => {
+                println!("Error during consumer join. error: {:?}",err)
+            }
+            _ => {}
+        }
     }
 }
